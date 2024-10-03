ad019acedf227db04c5b7beb788bc11a8e6180ee
brajesh708 <brajeshmewada694@gmail.com>
2024-10-03 09:59:36 +0530

    create models

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class EmployeData(models.Model):
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField()
+    address=models.CharField(max_length=200)
+    selary=models.IntegerField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+                             
+                             
